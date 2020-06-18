b19ae20b26bd6796f792bb1ed955f54006f8ab44
Carl Eiserman <carleiserman@gmail.com>
2020-06-18 11:10:39 +0200

    fix: css indentation

--- src/components/buttons/button/styles.ts
+++ src/components/buttons/button/styles.ts
@@ -15,108 +15,107 @@ interface IStyledButtonWrapperProps {
 
 export const StyledButtonWrapper = styled.button<IStyledButtonWrapperProps>`
   ${(props: IStyledButtonWrapperProps) => {
-    const { 
-      disabled,
-      hasOutline,
-      isActive,
-      styleVariant,
-      theme,
-    } = props;
-    
-    console.log('received theme: ', theme);
 
-    return css<any>`
-      flex-direction: row;
-      align-items: center;
-      font-size: ${BASE_FONT_SIZE * 0.825}rem;
-      color: ${theme.constants.primary};
-      background-color: ${theme.constants.primaryLight};
-      height: 1.5rem;
-      // margin: 0 0.25rem;
-      justify-content: center;
-      vertical-align: center;
-      overflow: hidden;
-      user-select: none;
-      border-radius: 1rem;
-      display: flex;
-      outline: none;
-      padding: 0 1em;
-      cursor: pointer;
+  const {
+    disabled,
+    hasOutline,
+    isActive,
+    styleVariant,
+    theme,
+  } = props;
 
-      border: none;
 
-      transition: background-color 150ms linear, color 150ms linear;
+  return css`
+    flex-direction: row;
+    align-items: center;
+    font-size: ${BASE_FONT_SIZE * 0.825}rem;
+    color: ${theme.constants.primary};
+    background-color: ${theme.constants.primaryLight};
+    height: 1.5rem;
+    // margin: 0 0.25rem;
+    justify-content: center;
+    vertical-align: center;
+    overflow: hidden;
+    user-select: none;
+    border-radius: 1rem;
+    display: flex;
+    outline: none;
+    padding: 0 1em;
+    cursor: pointer;
 
-      &:hover, &:active {
-        background-color: ${theme.constants.primary};
+    border: none;
+
+    transition: background-color 150ms linear, color 150ms linear;
+
+    &:hover, &:active {
+      background-color: ${theme.constants.primary};
+      color: ${theme.constants.primaryLightBlue};
+
+      > svg {
+        fill: ${theme.constants.primaryLightBlue};
+        stroke: ${theme.constants.primaryLightBlue};
+      }
+    }
+
+    &:link, &:visited, &:active {
+      color: ${theme.constants.primaryLight};
+    }
+
+    ${isActive && css`
+      background-color: ${theme.constants.primary};
+      color: ${theme.constants.primaryLight};
+      &:hover {
         color: ${theme.constants.primaryLightBlue};
-
-        > svg {
-          fill: ${theme.constants.primaryLightBlue};
-          stroke: ${theme.constants.primaryLightBlue};
-        }
       }
+    `}
 
-      &:link, &:visited, &:active {
-        color: ${theme.constants.primaryLight};
+    ${hasOutline && css`
+      background-color: transparent;
+      border: 1px solid ${theme.constants.primaryLight};
+      color: ${theme.constants.primaryLight};
+
+      &:hover {
+        border: 1px solid ${theme.constants.primaryLightBlue};
+        color: ${theme.constants.primaryLightBlue};
       }
+    `}
 
-      ${isActive && css`
-        background-color: ${theme.constants.primary};
-        color: ${theme.constants.primaryLight};
-        &:hover {
-          color: ${theme.constants.primaryLightBlue};
-        }
-      `}
+    ${disabled && css`
+      color: ${theme.constants.primaryLight};
+      background-color: ${theme.constants.border};
+      border-color: ${theme.constants.border};
+      cursor: not-allowed;
 
-      ${hasOutline && css`
-        background-color: transparent;
-        border: 1px solid ${theme.constants.primaryLight};
-        color: ${theme.constants.primaryLight};
-
-        &:hover {
-          border: 1px solid ${theme.constants.primaryLightBlue};
-          color: ${theme.constants.primaryLightBlue};
-        }
-      `}
-
-      ${disabled && css`
+      &:hover {
         color: ${theme.constants.primaryLight};
         background-color: ${theme.constants.border};
         border-color: ${theme.constants.border};
-        cursor: not-allowed;
+      }
+    `}
 
-        &:hover {
-          color: ${theme.constants.primaryLight};
-          background-color: ${theme.constants.border};
-          border-color: ${theme.constants.border};
-        }
-      `}
+    ${(styleVariant === BUTTON_STYLE_VARIANTS.DEFAULT) && css`
+      font-size: 12px;
+      letter-spacing: 0;
+      // text-transform: uppercase;
+      box-shadow: 1px 1px 1px #00000034;
+      border-radius: 20px;
+    `}
 
-      ${(styleVariant === BUTTON_STYLE_VARIANTS.DEFAULT) && css`
-        font-size: 12px;
-        letter-spacing: 0;
-        // text-transform: uppercase;
-        box-shadow: 1px 1px 1px #00000034;
-        border-radius: 20px;
-      `};
+    ${(styleVariant === BUTTON_STYLE_VARIANTS.LONG) && css`
+      min-width: 108px;
+      height: 20px;
+      font-size: 12px;
+      letter-spacing: 0;
+      text-transform: uppercase;
+      box-shadow: 1px 1px 1px #00000034;
+      border-radius: 20px;
+    `}
 
-      ${(styleVariant === BUTTON_STYLE_VARIANTS.LONG) && css`
-        min-width: 108px;
-        height: 20px;
-        font-size: 12px;
-        letter-spacing: 0;
-        text-transform: uppercase;
-        box-shadow: 1px 1px 1px #00000034;
-        border-radius: 20px;
-      `};
-
-      ${(styleVariant === BUTTON_STYLE_VARIANTS.LEGACY) && css`
-        box-shadow: 1px 1px 0 1px rgba(0,0,0,0.08);
-      `};
-    `;
-  }}
-`;
+    ${(styleVariant === BUTTON_STYLE_VARIANTS.LEGACY) && css`
+      box-shadow: 1px 1px 0 1px rgba(0,0,0,0.08);
+    `}
+  `;
+}}`;
 
 export const StyledButtonContentWrapper = styled.div`
   display: flex;
@@ -124,6 +123,3 @@ export const StyledButtonContentWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
-// StyledButtonWrapper.defaultProps = { theme };
-// StyledButtonContentWrapper.defaultProps = { theme };
