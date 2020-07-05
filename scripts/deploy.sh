4fac67d9b02141e97681df7646a153f567f2d2fd
Carl Eiserman <carleiserman@gmail.com>
2020-07-05 18:09:11 +0200

    chore: deployment scripts 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,12 @@ yarn build-storybook
 echo "moving storybook folder..."
 mv ./storybook-static ../storybook-static
 git branch -D deploy/temp
+git checkout -b deploy/temp
 rm -rf .storybook config public src .gitignore .npmignore .npmrc package.json tsconfig.json yarn.lock
 mv ../storybook-static/* ./*
 git add .
 git commit -m "feat: deployed storybook"
 git push github gh-pages -f
 git checkout dev
+git branch -D deploy/temp
+pause
